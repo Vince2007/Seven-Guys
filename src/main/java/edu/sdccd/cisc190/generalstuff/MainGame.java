71706c31e5076814a5056cf25604ccc4e7e9fcdc
ooo1410 <tonyguz5321@gmail.com>
2024-12-07 23:22:45 -0800

    Update MainGame.java

--- src/main/java/edu/sdccd/cisc190/generalstuff/MainGame.java
+++ src/main/java/edu/sdccd/cisc190/generalstuff/MainGame.java
@@ -7,12 +7,14 @@ public class MainGame extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        // TODO: Add a comment explaining that this is where the main scenes for the game are created (e.g., title screen, main menu, game screen).
         // Create scenes for each part of the game
         TitleScreen titleScreen = new TitleScreen(primaryStage);
         MainMenu mainMenu = new MainMenu(primaryStage);
         PreLude gameScreen = new PreLude(primaryStage);
 
         // Set the title screen as the initial scene
+        // TODO: Explain that this line sets the initial screen that the player will see when they open the game (title screen).
         primaryStage.setTitle("NightShift at Seven Guys (UNFINISHED VERSION)");
         primaryStage.setScene(titleScreen.getScene());
         primaryStage.show();
